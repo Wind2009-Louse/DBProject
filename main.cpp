78c1125a9ae211dae1a5b19cf4b742669ea2d62a
Wind2009-Louse <wind.jackey@qq.com>
2019-12-03 13:53:59 +0800

    Update main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,60 @@
+#include "dbupdate.h"
 #include "pointer_search.h"
 #include "range_search.h"
 
 int main() {
 	Container* ctr = new Container();
-	Insert_into_Container(ctr, "aa");
-	Insert_into_Container(ctr, "a");
-	Insert_into_Container(ctr, "ac");
-	Insert_into_Container(ctr, "ab");
-	Insert_into_Container(ctr, "ada");
-	Insert_into_Container(ctr, "aaa");
-	Insert_into_Container(ctr, "aaaaa");
+	ctr->cptrs->head_ptr = ctr;
+	
+	// input
+	while (true) {
+		string ip;
+		cin >> ip;
+		if (ip == "0") {
+			break;
+		}
+		Insert_into_db(ctr, ip.c_str());
+	}
 
-	cout << sizeof(Container) << endl;
-	cout << sizeof(Node) << endl;
+	// remove
+	while (true) {
+		string ip;
+		cin >> ip;
+		if (ip == "0") {
+			break;
+		}
+		cout << (Delete_in_db(ctr, ip.c_str()) ? "True" : "False") << endl;
+	}
 
-	system("pause");
+	// point test
+	while (true) {
+		string ip;
+		cin >> ip;
+		if (ip == "0") {
+			break;
+		}
+		bool result = Pointsearch_in_db(ctr, ip.c_str());
+		cout << (result ? "True" : "False") << endl;
+	}
+
+	// range test
+	string lower = "", upper = "";
+	bool no_upper = true;
+	while (true) {
+		vector<string> results = Rangesearch_in_db(ctr, lower.c_str(), upper.c_str(), true, true, no_upper);
+		//cout << "Results:" << endl;
+		for (int i = 0; i < results.size(); ++i) {
+			//cout << '\t' << results[i] << endl;
+			cout << results[i] << endl;
+		}
+
+		no_upper = false;
+		cin >> lower >> upper;
+		if (lower == "0" && upper == "0") {
+			break;
+		}
+	}
+
+	Print_Container(ctr);
+	//system("pause");
 }
